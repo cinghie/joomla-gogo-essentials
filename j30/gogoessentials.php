2f7e6ee4223b5d87c940a88a8cd8b975f40caad6
Giandomenico Olini <giando.olini@gogodigital.it>
2016-05-20 01:42:17 +0200

    Adding Google Analytics Code

--- j30/gogoessentials.php
+++ j30/gogoessentials.php
@@ -16,8 +16,37 @@ defined( '_JEXEC' ) or die;
 
 class plgSystemGogoessentials extends JPlugin
 {
+
     public function __construct( &$subject, $config )
     {
         parent::__construct( $subject, $config );
     }
+
+    public function onBeforeCompileHead()
+    {
+        if ($this->params->get('googleAnaliytics_admin') == false) {
+            $app = JFactory::getApplication();
+
+            if ($app->isAdmin()) {
+                return;
+            }
+        }
+
+        if($this->params->get('googleAnalytics_id'))
+        $this->addingTrackingCode($this->params->get('googleAnalytics_id'));
+    }
+
+    protected function addingTrackingCode($trackingid)
+    {
+        $script = "\n\t"."(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){";
+        $script.= "\n\t"."(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),";
+        $script.= "\n\t"."m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)";
+        $script.= "\n\t"."})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
+        $script.= "\n\t"."ga('create', '".$trackingid."', 'auto');";
+        $script.= "\n\t"."ga('send', 'pageview');";
+
+        $document = JFactory::getDocument();
+        $document->addScriptDeclaration($script);
+    }
+
 }
