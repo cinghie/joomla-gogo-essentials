1ee2027a21175d42578923d839a4ad8d2a3f5d05
Giandomenico Olini <giando.olini@gogodigital.it>
2016-05-19 23:28:12 +0200

    First commit

---
+++ j30/gogoessentials.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @email info@gogodigital.it
+ * @github https://github.com/cinghie/joomla-gogo-essentials
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 2
+ * @package Joomla Gogodigital Essentials
+ * @version 3.0.0
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die;
+
+class plgSystemGogoessentials extends JPlugin
+{
+    public function __construct( &$subject, $config )
+    {
+        parent::__construct( $subject, $config );
+    }
+}
