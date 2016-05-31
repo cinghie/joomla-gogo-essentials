d4a356becc4cdc7b2efacf49aeddf5b570194c5b
Giandomenico Olini <giando.olini@gogodigital.it>
2016-06-01 00:02:48 +0200

    Update english language

--- j30/gogoessentials.php
+++ j30/gogoessentials.php
@@ -8,7 +8,7 @@
  * @github https://github.com/cinghie/joomla-gogo-essentials
  * @license GNU GENERAL PUBLIC LICENSE VERSION 2
  * @package Joomla Gogodigital Essentials
- * @version 3.0.4
+ * @version 3.0.5
  */
 
 // no direct access
